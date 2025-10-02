34c46072efeead8d93a1177823ffc25c6e3a60a3
shabunovasofia <63457717+shabunovasofia@users.noreply.github.com>
2025-10-02 22:44:24 +0000

    добавляем file.txt, script.sh, numbers.txt,average.sh

--- lab1/src/average.sh
+++ lab1/src/average.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
+
 sum=0
-if [ $# -eq 0 ]; then
-    echo "Ошибка: Не передано ни одного числа!"
-    exit 1
-fi
 count=$#
+
 for num in "$@"; do
     sum=$((sum + num))
 done
@@ -13,4 +11,3 @@ average=$((sum / count))
 
 echo "Количество: $count"
 echo "Среднее: $average"
-
